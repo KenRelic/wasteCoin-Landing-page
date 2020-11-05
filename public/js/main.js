9d0893f585c1f6feb1c7f600aa173f50762ac800
kenrelic <simeonkenneth3@gmail.com>
2020-11-05 08:19:17 +0100

    fix: web accessibility

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,21 @@ let menuLinks = document.querySelectorAll('.menu-items-section a');
 menuItemWrapper = Array.from(menuItemWrapper.children);
 
 menuIcon.addEventListener('click', displayMenu);
-menuLinks.forEach(link=> link.addEventListener('click', closeMenuBar))
+menuIcon.addEventListener('keyup', (e) => {
+  if (e.keyCode == 13 || e.which == 13) displayMenu();
+});
+menuLinks.forEach(link => link.addEventListener('click', () => {
+  preventTabndexing();
+  closeMenuBar();
+  return;
+}));
+menuLinks.forEach(link => link.addEventListener('keyup', (e) => {
+  if (e.keyCode == 13 || e.which == 13) {
+    preventTabndexing();
+    closeMenuBar();
+    return;
+  };
+}));
 
 window.onscroll = function (e) {
   let offset = window.pageYOffset;
@@ -20,16 +34,51 @@ function displayMenu() {
   navBar.style.height = '100vh';
   document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
   menuIcon.children[0].classList.replace('fa-bars', 'fa-times');
+  allowTabIndexing();
   menuIcon.removeEventListener('click', displayMenu);
   menuIcon.addEventListener('click', closeMenuBar);
+  menuIcon.removeEventListener('keyup', (e) => {
+    if (e.keyCode == 13 || e.which == 13) displayMenu();
+  })
+  menuIcon.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13 || e.which == 13) closeMenuBar();
+  })
 };
 
 function closeMenuBar() {
   navBar.style.height = '60px';
   document.getElementsByTagName('html')[0].style.overflowY = 'auto';
   menuIcon.children[0].classList.replace('fa-times', 'fa-bars');
+  preventTabndexing();
   menuIcon.removeEventListener('click', closeMenuBar);
   menuIcon.addEventListener('click', displayMenu);
+  menuIcon.removeEventListener('keyup', (e) => {
+    if (e.keyCode == 13 || e.which == 13) closeMenuBar();
+  })
+  menuIcon.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13 || e.which == 13) displayMenu();
+  })
+  return;
 };
 
+
+window.onresize = function () {
+  preventTabndexing();
+}
+function preventTabndexing() {
+  let mediaQuery = window.matchMedia('(max-width: 960px)');
+  if (mediaQuery.matches) {
+    return menuLinks.forEach(link => link.setAttribute('tabindex', -1))
+  }
+  closeMenuBar();
+  return menuLinks.forEach(link => link.setAttribute('tabindex', 1))
+}
+
+function allowTabIndexing() {
+  return menuLinks.forEach(link => link.setAttribute('tabindex', 1))
+}
+
+window.onload = function () {
+ return preventTabndexing();
+}
 
