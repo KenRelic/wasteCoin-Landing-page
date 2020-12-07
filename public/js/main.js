be5ddbd7aa2971ac2ffe0ec4ae2831443cb513d7
KenRelic <simeonkenneth3@gmail.com>
2020-12-08 00:52:31 +0100

    fix: update text content and brand color tweak

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,24 @@
 /*GLOBAL VARIABLES*/
 const menuIcon = document.querySelector('.menu-icon');
-const navBar = document.querySelector('.nav');
 let menuItemWrapper = document.querySelector('.menu-items-section');
-let menuLinks = document.querySelectorAll('.menu-items-section a');
+let menuLinks = document.querySelectorAll('.menu-items-section-mobile a');
 menuItemWrapper = Array.from(menuItemWrapper.children);
+let navBars = document.querySelectorAll('.nav');
+let mobileMenuItemWrapper = document.querySelector('.menu-items-section-mobile');
+navBars = Array.from(navBars);
 
 menuIcon.addEventListener('click', displayMenu);
 menuIcon.addEventListener('keyup', (e) => {
   if (e.keyCode == 13 || e.which == 13) displayMenu();
 });
 menuLinks.forEach(link => link.addEventListener('click', () => {
-  preventTabndexing();
+  // preventTabndexing();
   closeMenuBar();
   return;
 }));
 menuLinks.forEach(link => link.addEventListener('keyup', (e) => {
   if (e.keyCode == 13 || e.which == 13) {
-    preventTabndexing();
+    // preventTabndexing();
     closeMenuBar();
     return;
   };
@@ -25,16 +27,16 @@ menuLinks.forEach(link => link.addEventListener('keyup', (e) => {
 window.onscroll = function (e) {
   let offset = window.pageYOffset;
   if (offset > 80) {
-    return navBar.classList.replace('nav-style-dark', 'nav-style-light');
+    return navBars.forEach(nav => nav.classList.replace('nav-style-dark', 'nav-style-light'));
   };
-  return navBar.classList.replace('nav-style-light', 'nav-style-dark');
+  return navBars.forEach(nav => nav.classList.replace('nav-style-light', 'nav-style-dark'));
 };
 
 function displayMenu() {
-  navBar.style.height = '100vh';
+  mobileMenuItemWrapper.style.top = '0';
   document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
   menuIcon.children[0].classList.replace('fa-bars', 'fa-times');
-  allowTabIndexing();
+  // allowTabIndexing();
   menuIcon.removeEventListener('click', displayMenu);
   menuIcon.addEventListener('click', closeMenuBar);
   menuIcon.removeEventListener('keyup', (e) => {
@@ -46,10 +48,10 @@ function displayMenu() {
 };
 
 function closeMenuBar() {
-  navBar.style.height = '60px';
+  mobileMenuItemWrapper.style.top = '-102vh';
   document.getElementsByTagName('html')[0].style.overflowY = 'auto';
   menuIcon.children[0].classList.replace('fa-times', 'fa-bars');
-  preventTabndexing();
+  // preventTabndexing();
   menuIcon.removeEventListener('click', closeMenuBar);
   menuIcon.addEventListener('click', displayMenu);
   menuIcon.removeEventListener('keyup', (e) => {
@@ -63,22 +65,21 @@ function closeMenuBar() {
 
 
 window.onresize = function () {
-  preventTabndexing();
+  resetMobileNavbar();
 }
-function preventTabndexing() {
-  let mediaQuery = window.matchMedia('(max-width: 960px)');
+
+function resetMobileNavbar() {
+  let mediaQuery = window.matchMedia('(max-width: 868px)');
   if (mediaQuery.matches) {
-    return menuLinks.forEach(link => link.setAttribute('tabindex', -1))
+    return closeMenuBar();
   }
-  // closeMenuBar();
-  return menuLinks.forEach(link => link.setAttribute('tabindex', 1))
 }
 
-function allowTabIndexing() {
-  return menuLinks.forEach(link => link.setAttribute('tabindex', 1))
-}
+// function allowTabIndexing() {
+//   return menuLinks.forEach(link => link.setAttribute('tabindex', 1))
+// }
 
-window.onload = function () {
- return preventTabndexing();
-}
+// window.onload = function () {
+//   return preventTabndexing();
+// }
 
